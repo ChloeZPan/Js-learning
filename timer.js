1cee59376e4d575f2eadb1e51fd727c0fd160bee
ZEYI PAN <zeyipan0112@gmail.com>
2020-07-14 23:33:29 -0400

    files for timer

---
+++ timer.js
@@ -0,0 +1,51 @@
+class Timer {
+	constructor(durationInput, startButton, pauseButton, callbacks) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+		if (callbacks) {
+			this.onStart = callbacks.onStart;
+			this.onTick = callbacks.onTick;
+			this.onComplete = callbacks.onComplete;
+		}
+
+		this.startButton.addEventListener('click', this.start());
+		this.pauseButton.addEventListener('click', this.pause);
+	}
+
+	start = () => {
+		if (this.onStart) {
+			this.onStart(this.timeRemaining);
+		}
+		this.tick(); //immediately run one tick
+
+		this.intervalId = setInterval(this.tick, 50);
+	};
+
+	tick = () => {
+		if (this.timeRemaining <= 0) {
+			this.pause();
+			if (this.onComplete) {
+				this.onComplete();
+			}
+		} else {
+			this.timeRemaining = this.timeRemaining - 0.05;
+			if (this.onTick) {
+				this.onTick(this.timeRemaining);
+			}
+		}
+	};
+
+	pause = () => {
+		console.log('here');
+		clearInterval(this.intervalId);
+	};
+
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+
+	set timeRemaining(time) {
+		this.durationInput.value = time.toFixed(2);
+	}
+}
